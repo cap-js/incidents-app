96fd765e39e8131a95e39164198ddefc57707537
Robin de Silva Jayasinghe <1537697+rjayasinghe@users.noreply.github.com>
2023-09-12 17:00:22 +0200

    replace the faulty custom code
    the new code takes care that a conversation without a date is completed
    with the current date.

--- srv/processors-service.js
+++ srv/processors-service.js
@@ -3,28 +3,19 @@ const cds = require('@sap/cds')
 class ProcessorsService extends cds.ApplicationService {
   /** Registering custom event handlers */
   init() {
-    this.before("UPDATE", "Incidents", (req) => this.onUpdate(req));
-    this.after("READ", "Incidents", (data) => this.changeUrgencyDueToSubject(data));
-
-    return super.init();
+    this.before("SAVE", "Incidents", (req) => this.ensureTimestampIsPresent(req))
+    return super.init()
   }
 
-  changeUrgencyDueToSubject(data) {
-    if (data) {
-      const incidents = Array.isArray(data) ? data : [data];
-      incidents.forEach((incident) => {
-        if (incident.title?.toLowerCase().includes("urgent")) {
-          incident.urgency = { code: "H", descr: "high" };
+  async ensureTimestampIsPresent (req) {
+    if (req.data.conversations) {
+      for(const conversation of req.data.conversations) {
+        if(!conversation.timestamp) {
+            conversation.timestamp = new Date(Date.now())
         }
-      });
+      }
     }
   }
-
-  /** Custom Validation */
-  async onUpdate (req) {
-    const { status_code } = await SELECT.one(req.subject, i => i.status_code).where({ID: req.data.ID})
-    if (status_code === 'C')
-      return req.reject(`Can't modify a closed incident`)
-  }
 }
+
 module.exports = ProcessorsService
