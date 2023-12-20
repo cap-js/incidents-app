690dc9c3c63d2551d3c24f31887841ddbaa047a0
Daniel Hutzel <daniel.hutzel@sap.com>
2023-12-20 21:07:17 +0100

    cosmetics

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,4 +1,4 @@
 const cds = require("@sap/cds");
 cds.once('bootstrap', (app) => {
-  app.serve('/incidents/app').from('@capire/incidents', 'app/incidents/webapp')
+  app.serve('/incidents/app').from(__dirname,'/app/incidents/webapp')
 })
