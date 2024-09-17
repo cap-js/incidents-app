b70ab4bfbad165f5da2d0ebdef936a3b47b237aa
I543321 <amit.sahu01@sap.com>
2024-09-17 13:58:09 +0530

    Update attachments test

--- xmpls/attachments.test.js
+++ xmpls/attachments.test.js
@@ -1,123 +1,84 @@
-const { expect } = require("chai");
-const cds = require("@sap/cds");
-const axios = require("axios");
-const { urls } = require("./util/config");
-const { getAccessToken } = require("./utils.js");
+const cds = require('@sap/cds/lib')
+const { default: axios } = require('axios')
+const { GET, POST, DELETE, PUT, expect } = cds.test(__dirname + '../../')
 const { createReadStream } = cds.utils.fs;
 const { join } = cds.utils.path;
+axios.defaults.auth = { username: 'alice' }
 
-axios.defaults.baseURL = urls.baseURL;
+jest.setTimeout(11111)
 
-describe('REST API Tests', () => {
-  let accessToken;
-  
-  before(async () => {
-    accessToken = await getAccessToken();
-    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`; 
-  });
-
-  describe('Create a new incident', () => {
+describe('Test attachments service', () => {
     let draftId = null;
     let docId = null;
-    const newIncident = {
-      title: 'attachments working 14',
+
+  it('Create an incident ', async () => {
+    const { status, statusText, data } = await POST(`/odata/v4/processor/Incidents`, {
+      title: 'Urgent attention required !',
       status_code: 'N'
-    };
+    })
+    draftId = data.ID
+    expect(status).to.equal(201)
+    expect(statusText).to.equal('Created')
+  })
 
-    it('Create an incident', async () => {
-      try {
-        const res = await axios.post("/odata/v4/processor/Incidents", newIncident, {
-          headers: { 'Content-Type': 'application/json' }
-        });
-        expect(res.status).to.equal(201); // HTTP 201 Created status expected
-        draftId = res.data.ID;
-        expect(res.data).to.include.keys('ID', 'title', 'status_code');
-        expect(res.data.title).to.equal(newIncident.title);
-        expect(res.data.status_code).to.equal(newIncident.status_code);
-      } catch (error) {
-        console.error('Error creating incident:', error.response ? error.response.data : error.message);
-        throw error;
-      }
-    });
+  it('+ Activate the draft', async () => {
+    const response = await POST(
+      `/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=false)/ProcessorService.draftActivate`
+    )
+    expect(response.status).to.eql(201)
 
-    it('+ Activate the draft', async () => {
-      console.log(`Draft ID: ${draftId}`);
-      try {
-        const res = await axios.post(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=false)/ProcessorService.draftActivate`, {}, {
-          headers: { 'Content-Type': 'application/json' }
-        });
-        expect(res.status).to.equal(201); // HTTP 201 Created status expected
-      } catch (error) {
-        console.error('Error activating draft:', error.response ? error.response.data : error.message);
-        throw error;
-      }
-    });
+  })
 
-    it('Upload attachment to the incident', async () => {
-      try {
-        // Draft Edit
-        await axios.post(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=true)/ProcessorService.draftEdit`, 
-        { PreserveChanges: true }, { headers: { 'Content-Type': 'application/json' } });
 
-        const content = createReadStream(join(__dirname, "util/SolarPanelReport.pdf"));
-        const attachRes = await axios.post(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=false)/attachments`, 
+  describe('Test the file upload', () => {
+    it(`Should Close the Incident-${draftId}`, async () => {
+      const { status } = await POST(
+        `/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=true)/ProcessorService.draftEdit`,
         {
-          up__ID: draftId,
-          filename: "SolarPanelReport.pdf",
-          mimeType: "application/pdf",
-          status: "Clean",
-          createdAt: new Date(),
-        }, { headers: { 'Content-Type': 'application/json' } });
+          PreserveChanges: true
+        }
+      )
+ 
 
-        docId = attachRes.data.ID;
+    const content = createReadStream(join(__dirname, "util/SolarPanelReport.pdf"));
+    const attachRes = await POST(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=false)/attachments`, 
+    {
+      up__ID: draftId,
+      filename: "SolarPanelReport.pdf",
+      mimeType: "application/pdf",
+      status: "Clean",
+      createdAt: new Date(),
+    }, { headers: { 'Content-Type': 'application/json' } });
+    
+        console.log(attachRes);
+    docId = attachRes.data.ID;
+        console.log("doc id"+docId);
+    // Upload the file content with PUT
+    const uploadResp = await PUT(
+      `/odata/v4/processor/Incidents_attachments(up__ID=${draftId},ID=${docId},IsActiveEntity=false)/content`,
+      content,
+      { headers: { 'Content-Type': 'application/pdf' } }
+    );
+    expect(uploadResp.status).to.equal(204);
+    // add attachments here
 
-        // Upload the file content with PUT
-        const uploadResp = await axios.put(
-          `/odata/v4/processor/Incidents_attachments(up__ID=${draftId},ID=${docId},IsActiveEntity=false)/content`,
-          content,
-          { headers: { 'Content-Type': 'application/pdf' } }
-        );
-        expect(uploadResp.status).to.equal(204); // HTTP 204 No Content indicates success
+ 
+      const response = await POST(
+        `/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=false)/ProcessorService.draftActivate`
+      )
+      expect(response.status).to.eql(200)
+    })
+    
 
-        // Prepare draft actions
-        await axios.post(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=false)/ProcessorService.draftPrepare`, {
-          SideEffectsQualifier: "",
-        });
-      } catch (error) {
-        console.error('Error uploading attachment:', error.response ? error.response.data : error.message);
-        throw error;
-      }
-    });
+  })
+  it('- check the uploaded file', async () => {
+    const response = await GET(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=true)/attachments(up__ID=${draftId},ID=${docId},IsActiveEntity=true)/content`);
+    expect(response.status).to.equal(200);
+    expect(response.data).to.not.be.undefined;
+  })
 
-    it('+ Activate the draft again', async () => {
-      try {
-        const res = await axios.post(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=false)/ProcessorService.draftActivate`, {});
-        expect(res.status).to.equal(200); // Expecting HTTP 200 status on final activation
-      } catch (error) {
-        console.error('Error activating draft:', error.response ? error.response.data : error.message);
-        throw error;
-      }
-    });
-
-    it('Read the file content', async () => {
-      try {
-        const response = await axios.get(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=true)/attachments(up__ID=${draftId},ID=${docId},IsActiveEntity=true)/content`);
-        expect(response.status).to.equal(200);
-        expect(response.data).to.not.be.undefined;
-      } catch (error) {
-        console.error('Error reading file content:', error.response ? error.response.data : error.message);
-        throw error;
-      }
-    });
-
-    it('- Delete the Incident', async () => {
-      try {
-        const res = await axios.delete(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=true)`);
-        expect(res.status).to.equal(204); // HTTP 204 No Content expected
-      } catch (error) {
-        console.error('Error deleting incident:', error.response ? error.response.data : error.message);
-        throw error;
-      }
-    });
-  });
-});
+  it('- Delete the Incident', async () => {
+    const response = await DELETE(`/odata/v4/processor/Incidents(ID=${draftId},IsActiveEntity=true)`)
+    expect(response.status).to.eql(204)
+  })
+})
