8898ad99231b8e5eb1845edf875bf391a36577bd
Daniel Hutzel <daniel.hutzel@sap.com>
2025-03-17 16:56:49 +0100

    simplified processor service impl

--- srv/processor-service.js
+++ srv/processor-service.js
@@ -1,30 +1,20 @@
 const cds = require('@sap/cds')
 
 class ProcessorService extends cds.ApplicationService {
-  /** Registering custom event handlers */
   init() {
-    this.before('UPDATE', 'Incidents', req => this.onUpdate(req))
-    this.before(['CREATE', 'UPDATE'], 'Incidents', req => this.changeUrgencyDueToSubject(req.data))
+
+    const { Incidents } = this.entities
+
+    this.before ('UPDATE', Incidents, async req => {
+      const closed = await SELECT.one(1) .from (req.subject) .where `status.code = 'C'`
+      if (closed) req.reject `Can't modify a closed incident!`
+    })
+
+    this.before (['WRITE'], Incidents, ({data}) => {
+      if (data.title?.match(/urgent/)) data.urgency_code = 'H'
+    })
+
     return super.init()
-  }
-
-  changeUrgencyDueToSubject(data) {
-    if (data) {
-      const incidents = Array.isArray(data) ? data : [data]
-      incidents.forEach(incident => {
-        if (incident.title?.toLowerCase().includes('urgent')) {
-          incident.urgency = { code: 'H', descr: 'High' }
-        }
-      })
-    }
-  }
-
-  /** Custom Validation */
-  async onUpdate(req) {
-    const { status_code } = await SELECT.one(req.subject, i => i.status_code).where({ ID: req.data.ID })
-    if (status_code === 'C') {
-      return req.reject(`Can't modify a closed incident`)
-    }
   }
 }
 
