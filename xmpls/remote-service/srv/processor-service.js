0e452cdcda98b62b794e27ee862aa2a4daea7579
i325261 <anirudh.prasad@sap.com>
2026-02-03 15:07:36 +0100

    Enhance project structure and configuration for incidents application

--- xmpls/remote-service/srv/processor-service.js
+++ xmpls/remote-service/srv/processor-service.js
@@ -27,6 +27,7 @@ class ProcessorService extends cds.ApplicationService {
       const customer = await this.S4bupa.run(SELECT.one(BusinessPartner, bp => {
         bp('*');
           bp.addresses(address => {
+            address('email', 'phoneNumber');
               address.email(emails => {
                 emails('email')
               });
